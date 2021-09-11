0699f7e00dfba3e554613aa6ac208c78ae5487c2
bgoss206 <70728714+bgoss206@users.noreply.github.com>
2021-09-11 12:38:14 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'codingbrax@gmail.com'
-EMAIL_HOST_PASSWORD = 'codingForPaintWA'
+EMAIL_HOST_USER = #HIDDEN FOR PRIVACY
+EMAIL_HOST_PASSWORD = #HIDDEN FOR PRIVACY
 
